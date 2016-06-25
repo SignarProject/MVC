c45db343674d635608a8fcb21b854dbf5293eadf
anton-rakhanov <fantom_nox@hotmail.com>
2016-06-25 16:04:47 +0300

    SQL-Query-Key_Words-UpperCase

--- SignarBD/DB_Create.sql
+++ SignarBD/DB_Create.sql
@@ -1,49 +1,49 @@
 create table Users
 (
-[UserID] INT IDENTITY(1,1) not null PRIMARY KEY,
-[Name] NVARCHAR(35) not null, --http://stackoverflow.com/questions/30485/what-is-a-reasonable-length-limit-on-person-name-fields
-[Surname] NVARCHAR(35) not null,
-[Email] NVARCHAR(254) not null, --RFC standart
-[Login] NVARCHAR(254) not null, --RFC standart
-[Password] NVARCHAR(254) not null, --RFC standart
-[IsAdmin] BIT not null,
+[UserID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[Name] NVARCHAR(35) NOT NULL, --http://stackoverflow.com/questions/30485/what-is-a-reasonable-length-limit-on-person-name-fields
+[Surname] NVARCHAR(35) NOT NULL,
+[Email] NVARCHAR(254) NOT NULL, --RFC standart
+[Login] NVARCHAR(254) NOT NULL, --RFC standart
+[Password] NVARCHAR(254) NOT NULL, --RFC standart
+[IsAdmin] BIT NOT NULL,
 [Version] ROWVERSION,
 )
 go
 
 create table Projects
 (
-[ProjectID] INT IDENTITY(1,1) not null PRIMARY KEY,
-[Name] NVARCHAR(254) not null,
-[Prefix] NVARCHAR(10) not null, --prefix must be small
-[IsDeleted] BIT not null,
+[ProjectID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[Name] NVARCHAR(254) NOT NULL,
+[Prefix] NVARCHAR(10) NOT NULL, --prefix must be small
+[IsDeleted] BIT NOT NULL,
 [Version] ROWVERSION
 )
 go
 
 create table Bugs
 (
-[BugID] INT IDENTITY(1,1) not null PRIMARY KEY,
-[Subject] NVARCHAR(254) not null, --brief description/title
-[Description] NVARCHAR(MAX) not null, --can be as large as possible
-[AssigneeID] INT FOREIGN KEY references Users(UserID),
-[ProjectID] INT not null FOREIGN KEY references Projects(ProjectID),
-[CreationDate] DATETIME not null,
-[ModificationDate] DATETIME not null,
-[BugStatus] TINYINT not null,
-[Priority] TINYINT not null,
+[BugID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[Subject] NVARCHAR(254) NOT NULL, --brief description/title
+[Description] NVARCHAR(MAX) NOT NULL, --can be as large as possible
+[AssigneeID] INT FOREIGN KEY REFERENCES Users(UserID),
+[ProjectID] INT NOT NULL FOREIGN KEY REFERENCES Projects(ProjectID),
+[CreationDate] DATETIME NOT NULL,
+[ModificationDate] DATETIME NOT NULL,
+[BugStatus] TINYINT NOT NULL,
+[Priority] TINYINT NOT NULL,
 [Version] ROWVERSION
 )
 go
 
 create table Comments
 (
-[CommentID] INT IDENTITY(1,1) not null PRIMARY KEY,
+[CommentID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 [Text] NVARCHAR(MAX), --can be as large as possible, and if it's large there will be an expand button
-[CreationDate] DATETIME not null,
+[CreationDate] DATETIME NOT NULL,
 [ModificationDate] DATETIME,
-[UserID] INT FOREIGN KEY references Users(UserID),
-[BugID] INT not null FOREIGN KEY references Bugs(BugID),
+[UserID] INT FOREIGN KEY REFERENCES Users(UserID),
+[BugID] INT NOT NULL FOREIGN KEY REFERENCES Bugs(BugID),
 [Version] ROWVERSION
 )
 
@@ -51,9 +51,9 @@ go
 
 create table Attachments
 (
-[AttachmentID] INT IDENTITY(1,1) not null PRIMARY KEY,
-[BugID] INT not null FOREIGN KEY references Bugs(BugID),
-[Name] NVARCHAR(255) not null, --is common for many OS, --http://serverfault.com/questions/9546/filename-length-limits-on-linux
+[AttachmentID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[BugID] INT NOT NULL FOREIGN KEY REFERENCES Bugs(BugID),
+[Name] NVARCHAR(255) NOT NULL, --is common for many OS, --http://serverfault.com/questions/9546/filename-length-limits-on-linux
 [ContentPath] NVARCHAR(MAX), --http://stackoverflow.com/questions/4377740/database-design-preferred-field-length-for-file-paths
 [Version] ROWVERSION
 )
@@ -62,8 +62,8 @@ go
 
 create table UsersToProjects
 (
-[UserID] INT not null FOREIGN KEY references Users(UserID),
-[ProjectID] INT FOREIGN KEY (ProjectID) references Projects(ProjectID),
+[UserID] INT NOT NULL FOREIGN KEY REFERENCES Users(UserID),
+[ProjectID] INT FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
 CONSTRAINT UserProject PRIMARY KEY NONCLUSTERED ([UserID], [ProjectID]),
 [Version] ROWVERSION
 )
@@ -71,8 +71,8 @@ go
 
 create table Filters
 (
-[FilterID] INT IDENTITY(1,1) not null PRIMARY KEY,
-[UserID] INT not null FOREIGN KEY references Users(UserID),
+[FilterID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[UserID] INT NOT NULL FOREIGN KEY REFERENCES Users(UserID),
 [Title] NVARCHAR(254),
 [FilterContent] NVARCHAR(MAX), --xml representation of serialized class, length is not too much but we don't know it beforehand. Anyway MAX will not do any harm
 [Version] ROWVERSION
