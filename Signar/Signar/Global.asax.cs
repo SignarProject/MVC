a6ddc7e72c684261773d56ed7d07832f3c962bf5
Loganser <fedorovsergiy94@gmail.com>
2016-07-13 18:26:36 +0300

    Custom Page Not Found (broken)

--- Signar/Signar/Global.asax.cs
+++ Signar/Signar/Global.asax.cs
@@ -5,11 +5,26 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Signar.Controllers;
 
 namespace Signar
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_EndRequest()
+        {
+            if (Context.Response.StatusCode == 404)
+            {
+                Response.Clear();
+
+                var rd = new RouteData();
+                rd.Values["controller"] = "Error";
+                rd.Values["action"] = "Error404";
+
+                IController c = new ErrorController();
+                c.Execute(new RequestContext(new HttpContextWrapper(Context), rd));
+            }
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
