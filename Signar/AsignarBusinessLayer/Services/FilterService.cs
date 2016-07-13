c84bfe6e98bf53f2d3e022b9ed546697f23488d8
anton-rakhanov <fantom_nox@hotmail.com>
2016-07-13 13:01:41 +0300

    Repair FilterService

--- Signar/AsignarBusinessLayer/Services/FilterService.cs
+++ Signar/AsignarBusinessLayer/Services/FilterService.cs
@@ -25,17 +25,27 @@ namespace AsignarBusinessLayer.Services
 
         public bool CreateItem(FilterDTO newItem)
         {
-            throw new NotImplementedException();
+            Filter newFilter = _converter.FilterFromDTO(newItem);
+
+            _dbContext.Filters.Add(newFilter);
+            _dbContext.SaveChanges();
+
+            return true;
         }
 
         public bool DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            Filter filter = _dbContext.Filters.Find(id);
+
+            _dbContext.Filters.Remove(filter);
+            _dbContext.SaveChanges();
+
+            return true;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _dbContext.Dispose();
         }
 
         public ICollection<FilterDTO> GetAllItems()
@@ -45,17 +55,44 @@ namespace AsignarBusinessLayer.Services
 
         public FilterDTO GetItem(int id)
         {
-            throw new NotImplementedException();
+            Filter filter = _dbContext.Filters.Find(id);
+
+            FilterDTO filterDTO = _converter.FilterToDTO(filter);
+
+            return filterDTO;
         }
 
         public ICollection<FilterDTO> GetPage(int pageNumber, SortBy sortValue, int itemAtOnce)
         {
-            throw new NotImplementedException();
+            switch (sortValue)
+            {
+                case SortBy.Title:
+                    {
+                        ICollection<Filter> searchResult = _dbContext.Filters.AsNoTracking().OrderBy(f => f.Title).Skip(itemAtOnce * (pageNumber - 1)).Take(itemAtOnce).ToList();
+                        ICollection<FilterDTO> dtoResult = new HashSet<FilterDTO>();
+
+                        foreach (var filter in searchResult)
+                        {
+                            dtoResult.Add(_converter.FilterToDTO(filter));
+                        }
+
+                        return dtoResult;
+                    }
+                default:
+                    {
+                        throw new ArgumentException("Invalid SortBy parameters!");
+                    }
+            }
         }
 
         public bool UpdateItem(FilterDTO updatedItem)
         {
-            throw new NotImplementedException();
+            Filter filter = _dbContext.Filters.Find(updatedItem.FilterID);
+
+            filter.Title = updatedItem.Title;
+            filter.FilterContent = _converter.FilterFromDTO(updatedItem).FilterContent;
+
+            return true;
         }
     }
 }
